deb44582e40ace49222fa8e59a934169fa7b541c
willyanfx <willyan.fx@gmail.com>
2025-01-23 09:22:13 -0700

    start

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()],
+
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
